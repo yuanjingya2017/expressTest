48aaf86aa5a0a3946f05d7c27857b8ddfa78ae6d
yuan <jingyayuan@newbanker.cn>
2019-07-02 19:25:09 +0800

    第二节

--- controllers/test1.js
+++ controllers/test1.js
@@ -39,3 +39,39 @@ p.convert = function (key, val) {
     }
   })
 }
+p.walk1 = function (obj, keyword, callback) {
+  let val;
+  for(let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      val = obj[key]
+      // 这里进行判断 如果还没有遍历到最底层，继续new Observe
+      if (typeof val === 'object') {
+        new Observer(val);
+      }
+      if (keyword === key) {
+        this.convert1(key, val, callback)
+      }
+    }
+  }
+}
+p.convert1 = function (key, val, callback) {
+  console.log(callback)
+  Object.defineProperty(this.data, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      console.log('你访问了' + val)
+      return val
+    },
+    set: function (newval) {
+      console.log('你设置了' + key)
+      console.log('新的' + key + ' = ' + newval)
+      if (newval === val) return
+      val = newval
+      callback(val)
+    }
+  })
+}
+p.$watch = function (key, callback) {
+  this.walk1(this.data, key, callback)
+}
